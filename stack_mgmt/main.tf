e295a0bda975b3d6548de203d777259be90082f4
Petros Kolyvas <petros.kolyvas@commercetools.com>
2023-06-15 15:15:06 -0300

    fixed vars

--- stack_mgmt/main.tf
+++ stack_mgmt/main.tf
@@ -8,8 +8,5 @@ terraform {
 }
 
 provider "spacelift" {
-  api_key_endpoint = "https://dontpanic.app.spacelift.io"
-  api_key_id = var.spacelift_key_id
-  api_key_secret = var.spacelift_key_secret
 }
 
--- stack_mgmt/variables.tf
+++ stack_mgmt/variables.tf
@@ -1,5 +1,3 @@
-variable "spacelift_key_id" {}
-variable "spacelift_key_secret" {}
 variable "terraform_version" {
     type = number
     description = "The Terraform version number"
